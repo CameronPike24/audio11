5294d0a0c04c8668130364bb3f7bfe0b438589f7
CameronPike24 <115454082+CameronPike24@users.noreply.github.com>
2023-05-17 11:21:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,18 +4,38 @@ from kivy.properties import NumericProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy_garden.graph import Graph, LinePlot
 import numpy as np
+#from android_permissions import AndroidPermissions
+from android.permissions import request_permissions,Permission,check_permission
+
 
 from tools import AudioPlayer
-
+print("imported tools")
 
 class MainApp(App):
 
     def build(self):
+        print("request_permissions")
+        request_permissions([Permission.INTERNET, Permission.RECORD_AUDIO])
+        
+        #request_permissions([Permission.INTERNET, Permission.RECORD_AUDIO,
+        #                    Permission.READ_EXTERNAL_STORAGE,Permission.WRITE_EXTERNAL_STORAGE])
         self.app = MainGrid()       
+        print("self.app = MainGrid()")
 
         return self.app
+        
+    #def on_start(self, *args):
+        #self.dont_gc = AndroidPermissions(self.start_app)
+
+    #def start_app(self):
+        #self.dont_gc = None        
+        
+        
+        
+        
 
     def init_thread(self):
+        print("def init_thread")
         self.playback_thread = threading.Thread(target=self.app.player.run)
         # daemon threads don't wait for main thread
         self.playback_thread.setDaemon(True)
@@ -28,14 +48,15 @@ class MainApp(App):
         print("Playback Thread", self.playback_thread.native_id, "stopped")
 
 class MainGrid(BoxLayout):
+    
 
     zoom = NumericProperty(1)
-
+    print("class MainGrid")
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.samples = 512
         self.zoom = 1
-        
+        print("self.graph = Graph")
         self.graph = Graph(y_ticks_major=0.5,
                            x_ticks_major=64,
                            border_color=[0, 1, 1, 1],
@@ -46,6 +67,7 @@ class MainGrid(BoxLayout):
                            draw_border=False,
                            x_grid_label=True, y_grid_label=False)
         # Mono, Sampling Rate, Chunksize
+        print("self.player = AudioPlayer(1, 44100, self.samples)")
         self.player = AudioPlayer(1, 44100, self.samples)
         self.ids.modulation.add_widget(self.graph)
         self.plot_x = np.linspace(0, 1, self.samples)
@@ -54,15 +76,18 @@ class MainGrid(BoxLayout):
         self.old_freq = 0
         self.freq = 0
         # adds plot to the graph widget
+        print("self.graph.add_plot(self.plot)")
         self.graph.add_plot(self.plot)
         self.update_plot(1)
 
     def update_plot(self, freq):
+        print("def update_plot")
         self.plot_y = np.sin(2*np.pi*freq*self.plot_x)
         # draws plot
         self.plot.points = [(x, self.plot_y[x]) for x in range(self.samples)]
 
     def update_zoom(self, value):
+        print("def update_zoom")
         if value == '+' and self.zoom < 8:
             self.zoom *= 2
             self.graph.x_ticks_major /= 2
@@ -71,6 +96,7 @@ class MainGrid(BoxLayout):
             self.graph.x_ticks_major *= 2
 
     def play_result(self):
+        print("def play_result")
         if self.ids.play.state == 'down':
             self.ids.play.text = '[b]STOP[/b]'            
             App.get_running_app().init_thread()
--- tools.py
+++ tools.py
@@ -1,14 +1,18 @@
 import numpy as np
 from audiostream import get_output, AudioSample, get_input_sources, get_input
 from kivy.clock import Clock
+print("import libraries in tools.py")
+
 
 
 class AudioPlayer:
     def __init__(self, channels, rate, chunk_size):
         super().__init__()
+        print("class AudioPlayer:")
         self.rate = rate
         self.chunk_size = chunk_size
         # initialize the engine and get an output device; can be initialized only once
+        print("self.stream = get_output")
         self.stream = get_output(
             channels=channels, rate=rate, buffersize=chunk_size, encoding=16)
         # create instance of AudioSample to handle the audio stream (output) e.g. play and stop
@@ -34,19 +38,26 @@ class AudioPlayer:
         #import sys
         #importlib.reload(sys.modules['audiostream']) # reloads the audiostream module - thought this might solve the problem; it doesn't!!
         self.mic = get_input(callback=self.micCallback, rate=8000, source='default', buffersize=2048) # initialises the method get_input from the module with the properties required   
-        self.mic.start() # starts the method 'self.mic' recording audio data
+        print("self.mic = get_input")
+      
         
       
     def micCallback(self, buffer):
-        # method which is called by the method 'get_input' to store recorded audio data (each buffer of audio samples)
+        print("def micCallback")
+        # method which is called by the method 'get_input' to store recorded audio data (each buffer of audio samples)        
         self.audioData.append(buffer) # appends each buffer (chunk of audio data) to variable 'self.audioData'
-        print('size of frames: ' + len(self.audioData))
+        #print('size of frames: ' + str(len(self.audioData)))
+        print('size of frames: ' + str(len(buffer)))
+        #print ('got : ' + str(len(buf)))
 
-    def start(self):
+    #def start(self):
         # method which begins the process of recording the audio data
-        Clock.schedule_interval(self.readChunk, 1 / self.samples_per_second) # calls the method 'self.readChunk' to read and store each audio buffer (2048 samples) 60 times per second
+        #self.mic.start() # starts the method 'self.mic' recording audio data
+        #Clock.schedule_interval(self.readChunk, 1 / self.samples_per_second) # calls the method 'self.readChunk' to read and store each audio buffer (2048 samples) 60 times per second 
+
 
     def readChunk(self, sampleRate):
+        print("def readChunk")
         # method which coordinates the reading and storing of the bytes from each buffer of audio data (which is a chunk of 2048 samples)
         self.mic.poll()  # calls 'get_input(callback=self.mic_....)' to read content. This byte content is then dispatched to the callback method 'self.micCallback'
 
@@ -61,38 +72,49 @@ class AudioPlayer:
         
 
     def set_freq(self, freq):
+        print("def set_freq")
         self.old_freq = self.freq
         self.freq = freq
    
     def render_audio(self, pos, freq):
+        print("def render_audio")
         start = pos
         end = pos + self.chunk_size
         x_audio = np.arange(start, end) / self.rate
         return np.sin(2*np.pi*freq*x_audio)
 
     def fade_out(self, signal, length):
+        print("def fade_out")
         amp_decrease = np.linspace(1, 0, length)
         signal[-length:] *= amp_decrease
         return signal
 
     @staticmethod
     def get_bytes(chunk):
+        print("def get_bytes")
         # chunk is scaled and converted from float32 to int16 bytes
         return (chunk * 32767).astype('int16').tobytes()
     
     def write_audio_data(self):
+        print("def write_audio_data")
         self.chunk = self.get_bytes(self.chunk)
         # write bytes of chunk to internal ring buffer
         self.sample.write(self.chunk)
 
     def run(self):
+        print("def run")
         self.stream.add_sample(self.sample)
         self.sample.play()
         self.playing = True
         self.pos = 0
         self.freq_change = False
+        print("self.mic.start()")
+        self.mic.start() # starts the method 'self.mic' recording audio data
+        Clock.schedule_interval(self.readChunk, 1 / self.samples_per_second) # calls the method 'self.readChunk' to read and store each audio buffer (2048 samples) 60 times per second         
+        
 
         while self.playing:
+            print("while self.playing:")
             self.chunk = self.render_audio(self.pos, self.old_freq)
             self.pos += self.chunk_size
             
@@ -100,13 +122,14 @@ class AudioPlayer:
                 self.chunk = self.fade_out(self.chunk, 256)
                 self.pos = 0
                 self.old_freq = self.freq
-                
+            print("self.write_audio_data()") 
             self.write_audio_data()
 
         # self.chunk = self.fade_out(
         #     self.render_audio(self.pos, self.old_freq), 256)
         # self.write_audio_data()
         # important for threading otherwise a new thread cannot be initialized
+        print("self.sample.stop()")
         self.sample.stop()
 
     def stop(self):
