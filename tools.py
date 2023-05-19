2c6f2cceca9d8834e3678f8ccf93b7295b476ccd
CameronPike24 <115454082+CameronPike24@users.noreply.github.com>
2023-05-19 14:44:58 +0200

    Add files via upload

--- tools.py
+++ tools.py
@@ -45,7 +45,8 @@ class AudioPlayer:
         #self.mic = get_input(callback=self.micCallback, rate=22050, source='default', buffersize=2048) # Works but gets x\00x\00
         #self.mic = get_input(callback=self.micCallback, rate=22050, source='default') # With no buffer does not work no frames
         #self.mic = get_input(callback=self.micCallback, rate=22050, source='default', buffersize=512) # does not work at all
-        self.mic = get_input(callback=self.micCallback, rate=22050, source='default', buffersize=1024) # does not work at all
+        #self.mic = get_input(callback=self.micCallback, rate=22050, source='default', buffersize=1024) # does not work at all
+        self.mic = get_input(callback=self.micCallback, rate=22050, source='mic', buffersize=2048) # 
         #self.mic = get_input(callback=self.micCallback, source='default')
         print("self.mic = get_input")
       
