580283eec580f60bfcef92bb951210b72b627d42
CameronPike24 <115454082+CameronPike24@users.noreply.github.com>
2023-05-19 10:58:17 +0200

    Add files via upload

--- tools.py
+++ tools.py
@@ -37,7 +37,8 @@ class AudioPlayer:
         self.audioData = [] # creates a list to store the audio bytes recorded
         #import sys
         #importlib.reload(sys.modules['audiostream']) # reloads the audiostream module - thought this might solve the problem; it doesn't!!
-        self.mic = get_input(callback=self.micCallback, rate=8000, source='default', buffersize=2048) # initialises the method get_input from the module with the properties required   
+        #self.mic = get_input(callback=self.micCallback, rate=8000, source='default', buffersize=2048) # initialises the method get_input from the module with the properties required   
+        self.mic = get_input(callback=self.micCallback, source='default')
         print("self.mic = get_input")
       
         
